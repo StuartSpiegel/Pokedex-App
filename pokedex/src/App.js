f78cc103c7e3dbe6fbb7221489748eb71b812188
StuartSpiegel <48696059+StuartSpiegel@users.noreply.github.com>
2023-08-23 15:50:50 -0400

    Added Pokemon types

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import PokemonGrid from './PokemonGrid';
 
+// helper function to map required data from the detailed poke API response.
+// The types array is mapped to create a comma-separated string for displaying the Pokemon types.
+const mapPokemonData = (pokemon) => {
+  return {
+    id: pokemon.id,
+    name: pokemon.name,
+    types: pokemon.types.map((type) => type.type.name),
+    imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`,
+  };
+};
+
 const App = () => {
   const [pokemonData, setPokemonData] = useState([]);
   // use the useEffect hook to fetch data when the component mounts.
   useEffect(() => {
-    // Fetch a list of Pokemon from the PokeAPI
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151') // You can adjust the limit as needed
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
       .then((response) => response.json())
-      .then((data) => setPokemonData(data.results))
-      .catch((error) => console.error('Error fetching Pokemon data:', error));
+      .then((data) => {
+        const promises = data.results.map((pokemon) =>
+          fetch(pokemon.url)
+            .then((response) => response.json())
+        );
+  
+        Promise.all(promises)
+          .then((detailedData) => {
+            setPokemonData(detailedData.map(mapPokemonData));
+          })
+          .catch((error) => {
+            console.error('Error fetching Pokemon details:', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokemon list:', error);
+      });
   }, []);
   // passing pokemonData as a prop to the PokemonGrid component. 
   return (
--- pokedex/src/PokemonGrid.js
+++ pokedex/src/PokemonGrid.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import './PokemonGrid.css'; //import my CSS style sheet
+import './PokemonGrid.css'; // Import your CSS file for styling
 
 const PokemonGrid = ({ pokemonData }) => {
   return (
     <div className="pokemon-grid">
       {pokemonData.map((pokemon) => (
-        <div key={pokemon.name} className="pokemon-card">
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
-            alt={pokemon.name}
-          />
+        <div key={pokemon.id} className="pokemon-card">
+          <img src={pokemon.imageUrl} alt={pokemon.name} />
           <h3>{pokemon.name}</h3>
-          
-          <p>{pokemon.types}</p>
+          <p>Type: {pokemon.types.join(', ')}</p>
         </div>
       ))}
     </div>
